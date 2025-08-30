96de0e15168069625bb6a2b3f21118b68d5a649a
VincentDirac <1350709199@qq.com>
2025-08-31 02:01:30 +0800

    add gcc/clang builds in Windows

--- make-release.sh
+++ make-release.sh
@@ -9,19 +9,14 @@ releasepath=thtk-bin-$1
 echo Generating $releasepath
 rm -rf $releasepath
 mkdir $releasepath
-rm -rf $releasepath-pdbs
-mkdir $releasepath-pdbs
 
 for i in thanm thanm.old thecl thdat thmsg thstd; do
   echo $i
   echo .ds doc-default-operating-system thtk | groff -mdoc -Tutf8 -P-buoc - $i/$i.1 | unix2dos > $releasepath/README.$i.txt
-  cp build/$i/RelWithDebInfo/$i.exe $releasepath/
-  cp build/$i/RelWithDebInfo/$i.pdb $releasepath-pdbs/
+  cp build/$i/$i.exe $releasepath/
 done
-cp build/thtk/RelWithDebInfo/thtk.dll $releasepath/
-cp build/thtk/RelWithDebInfo/thtk.lib $releasepath/
-cp build/thtk/RelWithDebInfo/thtk.pdb $releasepath-pdbs/
-cp "$(cygpath "$VCToolsRedistDir")/${THTK_ARCH:-x86}/Microsoft.VC143.OPENMP/vcomp140.dll" $releasepath/
+cp build/thtk/libthtk.dll $releasepath/
+cp build/thtk/libthtk.dll.a $releasepath/
 
 copy_doc() {
   while [ "$1" != "" ]; do
@@ -32,4 +27,3 @@ copy_doc() {
 copy_doc COPYING.{libpng,zlib} COPYING README NEWS
 
 zip -r -9 $releasepath.zip $releasepath
-zip -r -9 $releasepath-pdbs.zip $releasepath-pdbs
