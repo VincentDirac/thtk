2cc37ed548c4b93114182cf99d5ae62fd233aee7
VincentDirac <98157433+VincentDirac@users.noreply.github.com>
2025-08-31 06:18:03 +0800

    Update make-release.sh

--- make-release.sh
+++ make-release.sh
@@ -17,7 +17,7 @@ for i in thanm thanm.old thecl thdat thmsg thstd; do
 done
 cp build/thtk/libthtk.dll $releasepath/
 cp build/thtk/libthtk.dll.a $releasepath/
-cp ＂${ steps.msys2.outputs.msys2-location }/ucrt64/bin/libjpeg-8.dll＂ $releasepath/
+cp /d/a/_temp/msys64/ucrt64/bin/libjpeg-8.dll $releasepath/
 
 copy_doc() {
   while [ "$1" != "" ]; do
