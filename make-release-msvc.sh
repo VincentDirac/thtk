d3f677a3c588f8f557927fc2cbf3ce881d3320ed
VincentDirac <1350709199@qq.com>
2025-09-01 23:37:58 +0800

    Update

---
+++ make-release-msvc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+  echo "No version given."
+  exit 1
+fi
+
+releasepath=thtk-bin-$1
+echo Generating $releasepath
+rm -rf $releasepath
+mkdir $releasepath
+rm -rf $releasepath-pdbs
+mkdir $releasepath-pdbs
+
+for i in thanm thanm.old thecl thdat thmsg thstd; do
+  echo $i
+  echo .ds doc-default-operating-system thtk | groff -mdoc -Tutf8 -P-buoc - $i/$i.1 | unix2dos > $releasepath/README.$i.txt
+  cp build/$i/RelWithDebInfo/$i.exe $releasepath/
+  cp build/$i/RelWithDebInfo/$i.pdb $releasepath-pdbs/
+done
+cp build/thtk/RelWithDebInfo/thtk.dll $releasepath/
+cp build/thtk/RelWithDebInfo/thtk.lib $releasepath/
+cp build/thtk/RelWithDebInfo/thtk.pdb $releasepath-pdbs/
+
+copy_doc() {
+  while [ "$1" != "" ]; do
+    cat $1 | unix2dos > $releasepath/$1.txt
+    shift
+  done
+}
+copy_doc COPYING.{libpng,zlib} COPYING README NEWS
+
+zip -r -9 $releasepath.zip $releasepath
+zip -r -9 $releasepath-pdbs.zip $releasepath-pdbs
